968c0dc8305ee1c89fdd0b2b6878dc692be04536
greenboum-l <leahg8781@gmail.com>
2022-08-10 15:00:51 +0300

    Organization of TypeTask files

--- SchedulerConsoleApplication/TypeTask.c
+++ SchedulerConsoleApplication/TypeTask.c
@@ -1,88 +1,99 @@
 #include "TypeTask.h"
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
-
-
-#pragma region Defines
-#define READ "read"
-#define WRITE "write"
-#define DELETE "delete"
-
-#define TIME_TO_BYTE_PRIORITY1 0.1
-#define TIME_TO_BYTE_PRIORITY2 0.2
-#define TIME_TO_BYTE_PRIORITY3 0.3
-
-#define TIMEOUT_PRIORITY1 170
-#define TIMEOUT_PRIORITY2 440
-#define TIMEOUT_PRIORITY3 740
-#pragma endregion
-
 #pragma region Declarations of functions
-TypeTask* getTypeTaskByPriority(int priority);
+/// <summary>
+///Create TypeTask with all values, called only from the function getTypeTaskByPriority
+/// </summary>
+/// <param name="priority">priority of TypeTask</param>
+/// <param name="name">goal of TypeTask</param>
+/// <param name="timeToByte">time to byte of TypeTask</param>
+/// <param name="timeOut">time out TypeTask</param>
+/// <returns>pointer of TypeTask</returns>
 TypeTask* createTypeTask(int priority, char* name, double timeToByte, int timeOut);
-
-void printTypeTask(const TypeTask* typeTask);
-
-void freeTypeTask(TypeTask* typeTask);
-
+/// <summary>
+/// Set priority of TypeTask
+/// </summary>
+/// <param name="typeTask">pointer of TypeTask you want to insert a value into</param>
+/// <param name="priority">priority of this TypeTask</param>
 void setPriority(TypeTask* typeTask, int priority);
-int getPriority(TypeTask* typeTask);
-
+/// <summary>
+/// Set name of TypeTask
+/// </summary>
+/// <param name="typeTask">pointer of TypeTask you want to insert a value into</param>
+/// <param name="name">goal of this TypeTask</param>
 void setName(TypeTask* typeTask, char* name);
-char* getName(TypeTask* typeTask);
-
+/// <summary>
+/// Set time to byte of TypeTask
+/// </summary>
+/// <param name="typeTask">pointer of TypeTask you want to insert a value into</param>
+/// <param name="timeToByte">TimeToByte of this TypeTask</param>
 void setTimeToByte(TypeTask* ttypeTaskype, double timeToByte);
-double getTimeToByte(TypeTask* typeTask);
-
+/// <summary>
+/// Set time out of TypeTask
+/// </summary>
+/// <param name="typeTask">pointer of TypeTask you want to insert a value into</param>
+/// <param name="timeOut">TimeOut of this TypeTask</param>
 void setTimeOut(TypeTask* typeTask, int timeOut);
+/// <summary>
+/// Retrieving the time out of a TypeTask
+/// </summary>
+/// <param name="typeTask">- Pointer to TypeTask</param>
+/// <returns>Time Out of TypeTask</returns>
 int getTimeOut(TypeTask* typeTask);
-
+/// <summary>
+/// Checking whether the received value is valid
+/// </summary>
+/// <param name="name">string of goal of TypeTask</param>
+/// <returns>true or false</returns>
 bool isValidName(char* name);
+/// <summary>
+/// Checking whether the received value is valid
+/// </summary>
+/// <param name="timeToByte">timeToByte of TypeTask</param>
+/// <returns>true or false</returns>
 bool isValidTimeToByte(double timeToByte);
+/// <summary>
+/// Checking whether the received value is valid
+/// </summary>
+/// <param name="priority">priority of TypeTask</param>
+/// <returns>true or false</returns>
 bool isValidPriority(int priority);
+/// <summary>
+/// Checking whether the received value is valid
+/// </summary>
+/// <param name="timeOut">timeOut of TypeTask</param>
+/// <returns>true or false</returns>
 bool isValidTimeOut(int timeOut);
 
 #pragma endregion
 
-
-/* Based on the priority received, create a TypeTask */
 TypeTask* getTypeTaskByPriority(int priority) {
 	assert(isValidPriority(priority));
 	switch (priority)
 	{
-		case 1:
-			return createTypeTask(1, READ, TIME_TO_BYTE_PRIORITY1, TIMEOUT_PRIORITY1);
-		case 2:
-			return createTypeTask(2, WRITE, TIME_TO_BYTE_PRIORITY2, TIMEOUT_PRIORITY2);
-		case 3:
-			return createTypeTask(3, DELETE, TIME_TO_BYTE_PRIORITY3, TIMEOUT_PRIORITY3);
-		default:
-			printf("Priority %d not exist...", priority);
-			return NULL;
+	case 1:
+		return createTypeTask(1, READ, TIME_TO_BYTE_PRIORITY1, TIMEOUT_PRIORITY1);
+	case 2:
+		return createTypeTask(2, WRITE, TIME_TO_BYTE_PRIORITY2, TIMEOUT_PRIORITY2);
+	case 3:
+		return createTypeTask(3, DELETE, TIME_TO_BYTE_PRIORITY3, TIMEOUT_PRIORITY3);
+	default:
+		printf("Priority %d not exist...", priority);
+		return NULL;
 	}
 }
 
-
-/* Print all values of TypeTask */
 void printTypeTask(const TypeTask* typeTask) {
 	printf("typeTask { name: %s, priority:%d, time-to-byte: %f, timeOut: %d }",
 		getName(typeTask), getPriority(typeTask), getTimeToByte(typeTask), getTimeOut(typeTask));
 }
 
-
-/* Freeing dynamically allocated memory */
 void freeTypeTask(TypeTask* typeTask) {
 	assert(typeTask);
 	free(typeTask->name);
 	free(typeTask);
 }
 
-
-/* Create typeTask with values*/
 TypeTask* createTypeTask(int priority, char* name, double timeToByte, int timeOut) {
 	TypeTask* type = calloc(1, sizeof(TypeTask));
 	assert(type);
@@ -93,11 +104,9 @@ TypeTask* createTypeTask(int priority, char* name, double timeToByte, int timeOu
 	return type;
 }
 
-
 void setPriority(TypeTask* typeTask, int priority) {
 	assert(typeTask);
 	assert(isValidPriority(priority));
-	//assert(3 < priority || 0 > priority);
 	typeTask->priority = priority;
 }
 int getPriority(TypeTask* typeTask) {
@@ -134,7 +143,6 @@ int getTimeOut(TypeTask* typeTask) {
 	assert(typeTask);
 	return typeTask->timeOut;
 }
-
 
 inline bool isValidPriority(int priority) {
 	return 3 >= priority && 0 <= priority;
--- SchedulerConsoleApplication/TypeTask.h
+++ SchedulerConsoleApplication/TypeTask.h
@@ -1,6 +1,27 @@
 #pragma once
+#pragma region Defines
+#define READ "read"
+#define WRITE "write"
+#define DELETE "delete"
+
+#define TIME_TO_BYTE_PRIORITY1 0.1
+#define TIME_TO_BYTE_PRIORITY2 0.2
+#define TIME_TO_BYTE_PRIORITY3 0.3
+
+#define TIMEOUT_PRIORITY1 170
+#define TIMEOUT_PRIORITY2 440
+#define TIMEOUT_PRIORITY3 740
+#pragma endregion
+#pragma region Includes
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#pragma endregion
+
 
 struct TypeTask {
 	int priority;
@@ -8,8 +29,6 @@ struct TypeTask {
 	double timeToByte;
 	int timeOut;
 };
-
-
 typedef struct TypeTask TypeTask;
 
 /// <summary>
@@ -30,10 +49,21 @@ void printTypeTask(const TypeTask* typeTask);
 /// </summary>
 /// <param name="typeTask">- Pointer to TypeTask</param>
 void freeTypeTask(TypeTask* typeTask);
-
-
+/// <summary>
+/// Retrieving the priority of a TypeTask
+/// </summary>
+/// <param name="typeTask">- Pointer to TypeTask</param>
+/// <returns>Priority of TypeTask</returns>
 int getPriority(TypeTask* typeTask);
-
+/// <summary>
+/// Retrieving the purpuse of a TypeTask
+/// </summary>
+/// <param name="typeTask">- Pointer to TypeTask</param>
+/// <returns>The purpose of the TypeTask</returns>
 char* getName(TypeTask* typeTask);
-
+/// <summary>
+/// Retrieving the time to byte of a TypeTask
+/// </summary>
+/// <param name="typeTask">- Pointer to TypeTask</param>
+/// <returns>Time to byte of TypeTask</returns>
 double getTimeToByte(TypeTask* typeTask);
